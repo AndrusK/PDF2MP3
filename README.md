47487e44aabcc49bf59b0d78be94656e56f8e99a
AndrusK <bhayes.dev@hotmail.com>
2020-11-08 20:01:26 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+# pdf2mp3
+---
+The idea behind this project is to avoid paying for audiobooks. 
+I built this project with the inspiration of [Programming Hero's Video](https://www.youtube.com/watch?v=kyZ_5cvrXJI). You can find a link to his original project [here](https://github.com/ProgrammingHero1/audiobook). The only real difference between the one his project and mine is that mine generates a .mp3 file instead of reading it out.
+
+# Requirements
+---
+For me, I found that MacOS offers the best text-to-speech voices. With that said, this script is specifically targeted to MacOS.
+Some modifications would be needed to make it work with GNU/Linux or Windows. 
+
+I believe any version of Python 3 /should/ work, but don't quote me on that.
+
+# Installation
+---
+    pip3 -r requirements.txt
+
+# Usage
+---
+    pdf2mp3.py [-h] [-i (--pdf) <your_pdf.pdf>] [-o (--mp3) <output_file.mp3>] [-p (--start) <page # to start saving at>]
---
+++ pdf2mp3.py
@@ -0,0 +1,43 @@
+import argparse
+import pyttsx3
+import PyPDF2
+
+pdf = ""
+mp3 = ""
+start_page = ""
+
+parser = argparse.ArgumentParser(description='Generates .mp3 from .pdf files.')
+parser.add_argument("--pdf","-i", action='store', dest='pdf')
+parser.add_argument("--mp3","-o", action='store', dest='mp3')
+parser.add_argument("--start","-p", action='store', dest='start_page')
+args = parser.parse_args()
+
+if args.pdf is None or args.mp3 is None or args.start_page is None:
+    parser.error("One or more of the arguments were not used corretly.")
+if ".pdf" in args.pdf:
+    pdf = args.pdf
+else:
+    parser.error("PFD file must have the extention '.pdf'")
+if ".mp3" in args.mp3:
+    mp3 = args.mp3
+else:
+    mp3 = args.mp3 + ".mp3"
+if args.start_page.strip().isdigit():
+    start_page = int(args.start_page) - 1
+else:
+    parser.error('Page number was not properly formatted.')
+
+book = open(pdf, 'rb')
+pdfReader = PyPDF2.PdfFileReader(book)
+pages = pdfReader.numPages
+
+speaker = pyttsx3.init('nsss')
+voices = speaker.getProperty('voices')
+speaker.setProperty('voice', 'com.apple.speech.synthesis.voice.daniel')  
+
+whole_book_txt = ""
+
+for num in range(start_page, pages):
+    whole_book_txt += pdfReader.getPage(num).extractText()
+speaker.save_to_file(whole_book_txt, mp3)
+speaker.runAndWait()
